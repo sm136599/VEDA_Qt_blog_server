ef45e894011bfa4743c1d6ba31b33ff12ce85d37
수효 Soohyo <69195905+Dynamic-Seagull@users.noreply.github.com>
2024-10-23 17:17:14 +0900

    Create readjson.h

---
+++ readjson.h
@@ -0,0 +1,22 @@
+#ifndef READJSON_H
+#define READJSON_H
+
+#include <string>
+#include "json/json.h"
+
+// JSON 요청 파싱 함수
+bool parseRequest(const std::string &json_str, Json::Value &root);
+
+// 회원가입 요청을 파싱하는 함수
+bool parseJoinRequest(const std::string &json_str, std::string &loginId, std::string &password, std::string &passwordCheck, std::string &name);
+
+// 로그인 요청을 파싱하는 함수
+bool parseLoginRequest(const std::string &json_str, std::string &loginId, std::string &password);
+
+// 글쓰기 요청을 파싱하는 함수
+bool parsePostRequest(const std::string &json_str, std::string &title, std::string &writer, std::string &description);
+
+// 댓글 작성 요청을 파싱하는 함수
+bool parseCommentRequest(const std::string &json_str, int &postNumber, std::string &writer, std::string &description);
+
+#endif // READJSON_H
