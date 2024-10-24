2d0a0a2c63f61cd590545b476a8516793c42d1ec
수효 Soohyo <69195905+Dynamic-Seagull@users.noreply.github.com>
2024-10-24 20:13:18 +0900

    Update writejson.cpp

--- writejson.cpp
+++ writejson.cpp
@@ -10,6 +10,46 @@ std::string makeResponse(const std::string &status, const std::string &message) 
     return Json::writeString(writer, root);
 }
 
+std::string successJoin(const std::string &message) {
+    std::cout << "Join Success!\n";
+    Json::Value root;
+    root["status"] = "200";
+    root["result"] = "success";
+    root["message"] = message;
+
+    Json::StreamWriterBuilder writer;
+    return Json::writeString(writer, root);
+}
+
+std::string errorJoin(const std::string &message) {
+    std::cout << "Join Failed\n";
+    Json::Value root;
+    root["status"] = "400";
+    root["result"] = "failed";
+    root["message"] = message;
+
+    Json::StreamWriterBuilder writer;
+    return Json::writeString(writer, root);
+}
+
+std::string successLogin(const std::string &nickname) {
+    Json::Value root;
+    root["status"] = "200";
+    root["user"] = nickname;
+
+    Json::StreamWriterBuilder writer;
+    return Json::writeString(writer, root);
+}
+
+std::string failedLogin() {
+    Json::Value root;
+    root["status"] = "400";
+    root["user"] = "";
+
+    Json::StreamWriterBuilder writer;
+    return Json::writeString(writer, root);
+}
+
 std::string successResponse() {
     std::cout << "send message\n";
     return makeResponse("200", "success");
@@ -19,9 +59,15 @@ std::string errorResponse(const std::string &msg) {
     return makeResponse("400", msg);
 }
 
-std::string writePostsResponse(const Json::Value &posts) {
+std::string writePostsResponse(const std::vector<Json::Value> &posts) {
     Json::Value root;
-    root["posts"] = posts;
+    Json::Value postArray(Json::arrayValue);
+
+    for (const auto &post : posts) {
+        postArray.append(post);
+    }
+
+    root["posts"] = postArray;
 
     Json::StreamWriterBuilder writer;
     return Json::writeString(writer, root);
